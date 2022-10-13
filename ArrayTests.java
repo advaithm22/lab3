e71f0341af002d7688a48b7ab62ae0187cf8765c
Advaith Modali <advaith0331@gmail.com>
2022-10-13 09:14:29 -0700

    fixed methods that yielded failures

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceWith5Elements(){
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] {3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversedWith5Elements(){
+    int[] input1 = {1, 2, 3, 4, 5};
+    int[] newArray = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[] {5, 4, 3, 2, 1}, newArray);
+  }
 }
